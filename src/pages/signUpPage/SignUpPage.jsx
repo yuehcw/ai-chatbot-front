ed19617959251aac3bd43706b4bfa90ac59c785a
Yueh Chun Wu <yuehcw@uci.edu>
2024-09-08 18:50:41 -0700

    Fixing bugs

--- src/pages/signUpPage/SignUpPage.jsx
+++ src/pages/signUpPage/SignUpPage.jsx
@@ -5,7 +5,7 @@ import "./signUpPage.css";
 const SignUpPage = () => {
   return (
     <div className="signUpPage">
-      <SignUp path="/sign-up" routing="path" signInUrl="/sign-in"/>
+      <SignUp path="/sign-up" signInUrl="/sign-in"/>
     </div>
   );
 };
