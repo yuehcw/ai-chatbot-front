171a524e4afb442734f0d82a5f6f06159c49f000
Yueh Chun Wu <yuehcw@uci.edu>
2024-10-01 20:47:01 -0700

    Adding Testing account info

--- src/pages/signInPage/SignInPage.jsx
+++ src/pages/signInPage/SignInPage.jsx
@@ -5,7 +5,17 @@ import "./signInPage.css";
 const SignInPage = () => {
   return (
     <div className="signInPage">
-      <SignIn path="/sign-in" signUpUrl="/sign-up" forceRedirectUrl="/dashboard" />
+      <SignIn
+        path="/sign-in"
+        signUpUrl="/sign-up"
+        forceRedirectUrl="/dashboard"
+      />
+
+      <footer>
+        <p>Testing email: d1211773@gmail.com</p>
+        <br/>
+        <p>Testing password: 19980608!</p>
+      </footer>
     </div>
   );
 };
